385085648aa7f4986008faf959e78629dd091bd2
Dávid Komorowicz <david.komorowicz@gini.net>
2023-08-21 16:40:43 +0200

    Add config for ablation study variations for doctr custom

--- docmae/models/doctr_custom.py
+++ docmae/models/doctr_custom.py
@@ -14,28 +14,62 @@ from docmae.models.transformer import BasicEncoder, build_position_encoding
 
 
 class SelfAttnLayer(nn.Module):
-    def __init__(self, d_model, nhead=8, dim_feedforward=2048, dropout=0.1, activation="relu", normalize_before=False):
+    def __init__(
+        self,
+        d_model,
+        nhead=8,
+        dim_feedforward=2048,
+        dropout=0.1,
+        activation="relu",
+        normalize_before=False,
+        extra_attention=False,
+        extra_skip=False,
+    ):
         super().__init__()
+        self.extra_attention = extra_attention
+        self.extra_skip = extra_skip
         self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.cross_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
         # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.dropout = nn.Dropout(dropout)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
 
         self.norm1 = nn.LayerNorm(d_model)
-
+        self.norm2 = nn.LayerNorm(d_model)
         self.norm3 = nn.LayerNorm(d_model)
         self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
         self.dropout3 = nn.Dropout(dropout)
 
         self.activation = _get_activation_fn(activation)
         self.normalize_before = normalize_before
 
-    def forward_post(self, tgt):
+    def forward_post(self, tgt, pos_list=[None, None, None]):
+        tgt_orig = tgt  # for extra skip
         q = k = v = tgt
+
+        q = self.with_pos_embed(q, pos_list[0])
+        k = self.with_pos_embed(k, pos_list[1])
+        v = self.with_pos_embed(v, pos_list[2])
+
         tgt2 = self.self_attn(q, k, v, need_weights=False)[0]
-        tgt = q + self.dropout1(tgt2)
+        tgt = tgt + self.dropout1(tgt2)
         tgt = self.norm1(tgt)
+
+        if self.extra_attention:
+            q = k = v = tgt
+            if self.extra_attention:
+                k = tgt_orig
+                v = tgt_orig
+            q = self.with_pos_embed(q, pos_list[0])
+            k = self.with_pos_embed(k, pos_list[1])
+            v = self.with_pos_embed(v, pos_list[2])
+
+            tgt2 = self.cross_attn(q, k, v, need_weights=False)[0]
+            tgt = tgt + self.dropout2(tgt2)
+            tgt = self.norm2(tgt)
+
         tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
         tgt = tgt + self.dropout3(tgt2)
         tgt = self.norm3(tgt)
@@ -70,12 +104,15 @@ class SelfAttnLayer(nn.Module):
         tgt = tgt + self.dropout3(tgt2)
         return tgt
 
-    def forward(self, q):
+    def forward(self, q, pos_list):
         # if self.normalize_before:
         #     return self.forward_pre(
         #         tgt, memory_list, tgt_mask, memory_mask, tgt_key_padding_mask, memory_key_padding_mask, pos, memory_pos
         #     )
-        return self.forward_post(q)
+        return self.forward_post(q, pos_list=pos_list)
+
+    def with_pos_embed(self, tensor, pos: Optional[torch.Tensor]):
+        return tensor if pos is None else tensor + pos
 
 
 class CrossAttnLayer(nn.Module):
@@ -91,7 +128,6 @@ class CrossAttnLayer(nn.Module):
 
         self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
-
         self.norm3 = nn.LayerNorm(d_model)
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
@@ -101,33 +137,41 @@ class CrossAttnLayer(nn.Module):
         self.normalize_before = normalize_before
 
     def forward_post(
-        self,
-        tgt,  # embedding
-        cross_input,  # encoder features
+        self, tgt, cross_input, pos_list=[None, None, None]  # query embedding  # encoder features  # positional encoding
     ):
         q = k = v = tgt
+
+        q = self.with_pos_embed(q, pos_list[0])
+        k = self.with_pos_embed(k, pos_list[1])
+        v = self.with_pos_embed(v, pos_list[2])
+
         tgt2 = self.self_attn(q, k, v, need_weights=False)[0]
-        tgt = q + self.dropout1(tgt2)
+        tgt = tgt + self.dropout1(tgt2)
         tgt = self.norm1(tgt)
+
         tgt2 = self.cross_attn(
-            query=tgt,
-            key=cross_input,
-            value=cross_input,
+            query=self.with_pos_embed(tgt, pos_list[0]),
+            key=self.with_pos_embed(cross_input, pos_list[1]),
+            value=self.with_pos_embed(cross_input, pos_list[2]),
             need_weights=False,
         )[0]
         tgt = tgt + self.dropout2(tgt2)
         tgt = self.norm2(tgt)
+
         tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
         tgt = tgt + self.dropout3(tgt2)
         tgt = self.norm3(tgt)
         return tgt
 
-    def forward(self, q, cross_input):
+    def forward(self, q, cross_input, pos_list):
         # if self.normalize_before:
         #     return self.forward_pre(
         #         tgt, memory_list, tgt_mask, memory_mask, tgt_key_padding_mask, memory_key_padding_mask, pos, memory_pos
         #     )
-        return self.forward_post(q, cross_input)
+        return self.forward_post(q, cross_input, pos_list=pos_list)
+
+    def with_pos_embed(self, tensor, pos: Optional[torch.Tensor]):
+        return tensor if pos is None else tensor + pos
 
 
 def _get_clones(module, N):
@@ -146,8 +190,10 @@ def _get_activation_fn(activation):
 
 
 class TransDecoder(nn.Module):
-    def __init__(self, num_attn_layers, hidden_dim=128):
+    def __init__(self, num_attn_layers, hidden_dim=128, pos_encoding_before=True, pos_encoding_value=True):
         super(TransDecoder, self).__init__()
+        self.pos_encoding_before = pos_encoding_before
+        self.pos_encoding_value = pos_encoding_value
         attn_layer = CrossAttnLayer(hidden_dim)
         self.layers = _get_clones(attn_layer, num_attn_layers)
         position_embedding = build_position_encoding(hidden_dim)
@@ -157,21 +203,41 @@ class TransDecoder(nn.Module):
     def forward(self, imgf, query_embed):
         bs, c, h, w = imgf.shape
         imgf = imgf.flatten(2).permute(2, 0, 1)
-        # imgf = imgf + self.pos  # add positional encoding  # todo no pos encoding needed here?
         query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)
-        query_embed = query_embed + self.pos  # add positional encoding
+        if self.pos_encoding_before:
+            assert self.pos_encoding_value
+            # imgf = imgf + self.pos  # add positional encoding  # no pos encoding for embedding output originally
+            query_embed = query_embed + self.pos  # add positional encoding
+            pos_list = [None] * 3
+        elif self.pos_encoding_value:  # PE value and not PE query as in original
+            pos_list = [None, self.pos, self.pos]
+        else:  # don't encode value and encode query as in doctr
+            pos_list = [self.pos, self.pos, None]
 
         for layer in self.layers:
-            query_embed = layer(query_embed, cross_input=imgf)
+            query_embed = layer(query_embed, cross_input=imgf, pos_list=pos_list)
         query_embed = query_embed.permute(1, 2, 0).reshape(bs, c, h, w)
 
         return query_embed
 
 
 class TransEncoder(nn.Module):
-    def __init__(self, num_attn_layers, hidden_dim=128):
+    def __init__(
+        self,
+        num_attn_layers,
+        hidden_dim=128,
+        extra_attention=False,
+        extra_skip=False,
+        pos_encoding_before=True,
+        pos_encoding_value=True,
+    ):
         super(TransEncoder, self).__init__()
-        attn_layer = SelfAttnLayer(hidden_dim)
+        if not extra_attention:
+            assert not extra_skip
+
+        self.pos_encoding_before = pos_encoding_before
+        self.pos_encoding_value = pos_encoding_value
+        attn_layer = SelfAttnLayer(hidden_dim, extra_attention=extra_attention, extra_skip=extra_skip)
         self.layers = _get_clones(attn_layer, num_attn_layers)
         position_embedding = build_position_encoding(hidden_dim)
         self.pos = position_embedding(torch.ones((1, 36, 36), dtype=torch.bool).cuda())  # torch.Size([1, 128, 36, 36])
@@ -180,10 +246,16 @@ class TransEncoder(nn.Module):
     def forward(self, imgf):
         bs, c, h, w = imgf.shape
         imgf = imgf.flatten(2).permute(2, 0, 1)
-        imgf = imgf + self.pos  # add positional encoding
+        if self.pos_encoding_before:
+            imgf = imgf + self.pos
+            pos_list = [None] * 3
+        elif self.pos_encoding_value:
+            pos_list = [self.pos] * 3
+        else:
+            pos_list = [self.pos, self.pos, None]
 
         for layer in self.layers:
-            imgf = layer(imgf)
+            imgf = layer(imgf, pos_list=pos_list)
         imgf = imgf.permute(1, 2, 0).reshape(bs, c, h, w)
 
         return imgf
@@ -210,8 +282,20 @@ class DocTrOrig(nn.Module):
 
         self.fnet = BasicEncoder(output_dim=hdim, norm_fn="instance")
 
-        self.TransEncoder = TransEncoder(self.num_attn_layers, hidden_dim=hdim)
-        self.TransDecoder = TransDecoder(self.num_attn_layers, hidden_dim=hdim)
+        self.TransEncoder = TransEncoder(
+            self.num_attn_layers,
+            hidden_dim=hdim,
+            extra_attention=config["extra_attention"],
+            extra_skip=config["extra_skip"],
+            pos_encoding_before=config["pos_encoding_before"],
+            pos_encoding_value=config["pos_encoding_value"],
+        )
+        self.TransDecoder = TransDecoder(
+            self.num_attn_layers,
+            hidden_dim=hdim,
+            pos_encoding_before=config["pos_encoding_before"],
+            pos_encoding_value=config["pos_encoding_value"],
+        )
         self.query_embed = nn.Embedding(1296, hdim)
 
         self.flow_head = FlowHead(hdim, hidden_dim=hdim)
