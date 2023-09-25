18d46b30836f647b2542e316f2ce32f45b410bd4
Dávid Komorowicz <david.komorowicz@gini.net>
2023-09-25 17:19:15 +0200

    Integrate datamodule to train

--- docmae/train.py
+++ docmae/train.py
@@ -5,24 +5,18 @@ import argparse
 import shutil
 from pathlib import Path
 
+import gin
+import gin.torch.external_configurables
 import torch
 import lightning as L
 from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
 from lightning.pytorch.loggers import TensorBoardLogger
-from torch.utils.data import DataLoader
-import kornia.augmentation as ka
 
 # We are using BETA APIs, so we deactivate the associated warning, thereby acknowledging that
 # some APIs may slightly change in the future
 import torchvision
 
 torchvision.disable_beta_transforms_warning()
-
-import torchvision.transforms.v2 as transforms
-import torchvision.transforms as T
-
-from docmae.data.doc3d import Doc3D
-from docmae.data.docaligner import DocAligner
 
 from docmae import setup_logging
 from docmae.models.transformer import BasicEncoder
@@ -31,7 +25,7 @@ from docmae.models.doctr import DocTr
 from docmae.models.doctr_custom import DocTrOrig
 from docmae.models.doctr_plus import DocTrPlus
 from docmae.models.rectification import Rectification
-from docmae.data.augmentation.random_resized_crop import RandomResizedCropWithUV
+from docmae.datamodule.utils import init_external_gin_configurables
 
 logger = logging.getLogger(__name__)
 
@@ -39,6 +33,7 @@ logger = logging.getLogger(__name__)
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", required=True, type=Path, help="json config file for training parameters")
+    parser.add_argument("-d", "--data-config", required=True, type=Path, help="gin config file for the dataloader")
     parser.add_argument(
         "-ll",
         "--log-level",
@@ -53,68 +48,9 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def train(args, config: dict):
+@gin.configurable
+def train(args, config: dict, datamodule: L.LightningDataModule):
     L.seed_everything(config["training"]["seed"])
-
-    train_transform = transforms.Compose(
-        [
-            RandomResizedCropWithUV(
-                (288, 288), scale=(0.08, 1.0) if config["training"]["crop"] else (1.0, 1.0), antialias=True
-            ),
-            # ReplaceBackground(Path(config["background_path"]), "train1"),
-            transforms.ToImageTensor(),
-            transforms.ToDtype(torch.float32),
-        ]
-    )
-    image_transforms = T.Compose([
-        T.ConvertImageDtype(torch.float),
-        T.RandomChoice(
-            transforms=[
-                # change color
-                ka.RandomPlanckianJitter(keepdim=True),
-
-                ka.RandomPlasmaShadow(roughness=(0.1, 0.7), shade_intensity=(-0.25, 0), shade_quantity=(0, 0.5), p=1.0, keepdim=True),
-                ka.RandomPlasmaBrightness(roughness=(0.1, 0.7), intensity=(0.1, 0.5), p=1.0, keepdim=True),
-
-                ka.RandomInvert(p=1., keepdim=True),
-                ka.RandomPosterize(bits=4, p=1., keepdim=True),
-                # ka.RandomSharpness(p=1., keepdim=True),
-                ka.RandomAutoContrast(p=1., keepdim=True),
-                ka.RandomEqualize(p=1., keepdim=True),
-
-                ka.RandomGaussianBlur(kernel_size=(3, 5), sigma=(0.1, 1), p=1., keepdim=True),
-                ka.RandomMotionBlur(3, 35., 0.5, p=1., keepdim=True),
-            ],
-            p=[0.5, 0.25, 0.2,   0.05, 0.1, 0.05, 0.05, 0.1,  0.1, ],
-        ),
-        T.ConvertImageDtype(torch.uint8),
-    ])
-    val_transform = transforms.Compose(
-        [
-            RandomResizedCropWithUV(
-                (288, 288), scale=(0.08, 1.0) if config["training"]["crop"] else (1.0, 1.0), antialias=True
-            ),
-            # ReplaceBackground(Path(config["background_path"]), "val1"),
-            transforms.ToImageTensor(),
-            transforms.ToDtype(torch.float32),
-        ]
-    )
-    train_dataset = DocAligner(Path(config["dataset_path"]), "train", train_transform, image_transforms)
-    val_dataset = DocAligner(Path(config["dataset_path"]), "val", val_transform)
-    train_loader = DataLoader(
-        train_dataset,
-        config["training"]["batch_size"],
-        shuffle=True,
-        num_workers=config["training"]["num_workers"],
-        pin_memory=True,
-    )
-    val_loader = DataLoader(
-        val_dataset,
-        config["training"]["batch_size"],
-        shuffle=False,
-        num_workers=config["training"]["num_workers"],
-        pin_memory=True,
-    )
 
     callback_list = [
         LearningRateMonitor(logging_interval="step"),
@@ -158,7 +94,9 @@ def train(args, config: dict):
     # test export
     print(model.to_torchscript(method="trace"))
 
-    trainer.fit(model, train_loader, val_loader)
+    trainer.fit(model, datamodule=datamodule)
+
+    print(callback_list[1].best_model_path)
 
 
 def main():
@@ -171,7 +109,12 @@ def main():
     config_file = args.model_output_dir / "config.json"
     config = json.loads(args.config.read_text())
     shutil.copyfile(args.config, config_file)
-    train(args, config)
+
+    init_external_gin_configurables()
+    gin.parse_config_file(args.data_config)
+    Path(args.model_output_dir / "data_config.gin").write_text(gin.operative_config_str())
+
+    train(args, config, datamodule=gin.REQUIRED)
 
 
 if __name__ == "__main__":
